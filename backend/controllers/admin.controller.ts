9f12e6dd8d32c951db49c96fcf3386f38655073d
CakeVision <andreicodorean1811@gmail.com>
2024-12-03 19:05:39 +0200

    did minor refactoring

--- backend/controllers/admin.controller.ts
+++ backend/controllers/admin.controller.ts
@@ -7,41 +7,43 @@ export class AdminController {
         if(!('id' in req.body)){
             res.status(400).json({error:"Invalid req, no id to change user to provided"})
         }
-        if(!('newType' in req.body) ){
+        if(!('userType' in req.body) ){
             res.status(400).json({error:"Invalid req,no new userType provided"});
         }
-        const {id, newType} =  req.body
-        if(!User.isValidUserType(newType)){
+        const {id, userType} =  req.body
+        if(!User.isValidUserType(userType)){
             res.status(400).json({error:"Invalid req,not a correct userType"});
         }
-        User.update({userType: newType},{where:{id:id}});
+        User.update({userType: userType},{where:{id:id}});
         Token.update({refreshToken:"",accessToken:""},{where:{id:id}})
         res.status(200).json({"message":"update succesful"})
 
     }
+
     async delete(req,res){
-    const {id} = req.body
-    try{
-    Token.destroy({
-        where:{
-            id:id
+        const {id} = req.body
+        try{
+        Token.destroy({
+            where:{
+                id:id
+            }
+        })
+        User.destroy({
+            where:{
+                id:id
+            }
+        })
+        
+        res.status(200).json({"details":"delete succesful"})
         }
-    })
-    User.destroy({
-        where:{
-            id:id
+        catch{
+        res.status(500).json(
+            {
+                "status":"error"
+                "message":"delete failed"
+            }
+        );
         }
-    })
-    
-    res.status(200).json({"details":"delete succesful"})
-    }
-    catch{
-    res.status(400).json(
-        {
-            "message":"delete failed"
-        }
-    );
-    }
     }
 
 
