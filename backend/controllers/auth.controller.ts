2bcd07dee08755227cef3d0ff2ef9ed6740cec12
andreicodorean1811 <andreicodorean1811@gmail.com>
2025-01-06 16:09:37 +0200

    added user data to login response

--- backend/controllers/auth.controller.ts
+++ backend/controllers/auth.controller.ts
@@ -3,14 +3,15 @@ import { Request, Response } from 'express';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import { User, UserType } from '../models/user.model.ts';
-import {Token} from '../models/token.model.ts'
+import { Token } from '../models/token.model.ts'
+import { resolveAny } from 'dns';
 
 export class AuthController {
   async register(req: Request, res: Response) {
     try {
       console.log(req.body)
 
-      const { email, password, name,userType, managerId} = req.body;
+      const { email, password, name, userType, managerId } = req.body;
 
       const existingUser = await User.findOne({ where: { email } });
       if (existingUser) {
@@ -19,16 +20,16 @@ export class AuthController {
 
       const hashedPassword = await bcrypt.hash(password, 10);
       console.log(userType)
-      if(!User.isValidUserType(userType as string)){
-        return res.status(400).json({error: 'Invalid user type'})
+      if (!User.isValidUserType(userType as string)) {
+        return res.status(400).json({ error: 'Invalid user type' })
       }
       const user = await User.create(
         {
-          name:name,
-          email:email,
-          password:hashedPassword,
+          name: name,
+          email: email,
+          password: hashedPassword,
           userType: userType as UserType,
-          lastLogin:new Date(),
+          lastLogin: new Date(),
           managerId: managerId
         });
       console.log("New User: ")
@@ -41,8 +42,8 @@ export class AuthController {
       await Token.create(
         {
           id: user.getDataValue('id'),
-          accessToken:accessToken,
-          refreshToken:refreshToken
+          accessToken: accessToken,
+          refreshToken: refreshToken
         }
       )
       res.status(201).json({
@@ -60,7 +61,7 @@ export class AuthController {
     try {
       const { email, password } = req.body;
       console.log(email)
-      const user = await User.findOne({ where: { email:email } });
+      const user = await User.findOne({ where: { email: email } });
       if (!user) {
         console.log("no user found")
         return res.status(401).json({ error: 'Invalid credentials' });
@@ -74,7 +75,7 @@ export class AuthController {
 
       const { accessToken, refreshToken } = this.generateTokens(user);
 
-      await user.update({ 
+      await user.update({
         lastLogin: new Date()
       });
 
@@ -83,11 +84,14 @@ export class AuthController {
           refreshToken: refreshToken,
           accessToken: accessToken
         },
-        {where: {id: user.getDataValue('id')}}
+        { where: { id: user.getDataValue('id') } }
       )
-      res.json({
-        refreshToken
-      });
+      return res.status(200).json({
+        "status": "Success",
+        "message": `User succesfully logged in`,
+        "user": user,
+        "refreshToken": refreshToken,
+      })
     } catch (error) {
       res.status(500).json({ error: 'Error during login' });
     }
@@ -97,7 +101,7 @@ export class AuthController {
   async refresh(req: Request, res: Response) {
     try {
       console.log("Refresh ....................")
-      const authHeader  = req.headers.authorization;
+      const authHeader = req.headers.authorization;
       const refreshToken = authHeader?.split(' ')[1];
 
       if (!refreshToken) {
@@ -105,24 +109,24 @@ export class AuthController {
       }
 
       // Verify refresh token
-      const payload = jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET!) as { userId: number, userType:string };
+      const payload = jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET!) as { userId: number, userType: string };
       console.log(payload)
-    
+
       // Find user with this refresh token
       const validToken = await Token.findOne({
         where: {
           id: payload.userId,
-          refreshToken:refreshToken
+          refreshToken: refreshToken
         }
       });
 
-      const user = async (userId: string | number ):Promise<User> =>{
-        const result =  User.findOne({where:{id: userId}})!;
-        if(!result){
+      const user = async (userId: string | number): Promise<User> => {
+        const result = User.findOne({ where: { id: userId } })!;
+        if (!result) {
           throw new Error('User not found');
         }
         return result;
-      } 
+      }
       if (!validToken) {
         return res.status(401).json({ error: 'Invalid refresh token' });
       }
@@ -158,9 +162,9 @@ export class AuthController {
 
   private generateTokens(user: User) {
     const accessToken = jwt.sign(
-      { 
+      {
         userId: user.getDataValue('id'),
-        email:  user.getDataValue('email'), 
+        email: user.getDataValue('email'),
         userType: user.getDataValue('userType'),
       },
       process.env.JWT_SECRET!,
@@ -168,7 +172,7 @@ export class AuthController {
     );
 
     const refreshToken = jwt.sign(
-      { 
+      {
         userId: user.getDataValue('id'),
         userType: user.getDataValue('userType')
       },
