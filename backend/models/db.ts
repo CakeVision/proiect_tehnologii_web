9c6080203124a763a51ac886778e8f490331f76c
andreicodorean1811 <andreicodorean1811@gmail.com>
2025-01-08 10:26:05 +0200

    changed localhost in frontend to .env variable

--- backend/models/db.ts
+++ backend/models/db.ts
@@ -10,7 +10,6 @@ dotenv.config();
 console.log("Hello")
 const sequelize = new Sequelize(process.env.DATABASE_URL || '', {
   dialect: 'postgres',
-
   dialectOptions: {
     ssl: {
       require: true,
--- src/login/LoginPage.tsx
+++ src/login/LoginPage.tsx
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const LoginPage: React.FC = () => {
     const [email, setEmail] = useState("");
@@ -12,7 +15,7 @@ const LoginPage: React.FC = () => {
         setError(null);
 
         try {
-            const response = await fetch("http://localhost:3000/session/login", {
+            const response = await fetch(process.env.BACKEND_URL + "/session/login", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
