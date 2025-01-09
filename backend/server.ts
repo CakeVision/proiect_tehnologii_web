2ef6fbc49b2e79a605256175232e11674fc7301d
andreicodorean1811 <andreicodorean1811@gmail.com>
2025-01-09 09:21:12 +0200

    changed cors config in the server

--- backend/server.ts
+++ backend/server.ts
@@ -15,7 +15,7 @@ const app = express();
 const PORT = 3000;
 app.use(cors());
 app.options('*', (req, res) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.sendStatus(200);
